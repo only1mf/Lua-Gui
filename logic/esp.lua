afaa9b5ce8725ef4bd8e720d0419b37ae8a09903
CEO <155027833+only1mf@users.noreply.github.com>
2025-02-06 22:44:16 +0100

    Update esp.lua

---
+++ logic/esp.lua
@@ -0,0 +1,13 @@
+local esp = {}
+
+function esp.enable()
+    -- Add ESP activation logic here
+    print("ESP Enabled") -- Debugging message
+end
+
+function esp.disable()
+    -- Add ESP deactivation logic here
+    print("ESP Disabled") -- Debugging message
+end
+
+return esp
